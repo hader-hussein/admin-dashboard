daee4a216ea9f69f0d4f1cdc94b478dbff736190
hader-hussein <91197408+hader-hussein@users.noreply.github.com>
2023-10-22 12:07:44 +0300

    22/10/2023

--- assets/js/charts-apex.js
+++ assets/js/charts-apex.js
@@ -1485,4 +1485,151 @@
     const donutChart = new ApexCharts(donutChartEl, donutChartConfig);
     donutChart.render();
   }
+  // Donut Chart2
+  // --------------------------------------------------------------------
+  const donutChartEle = document.querySelector('#donutCharte'),
+    donutChartConfige = {
+      chart: {
+        height: 390,
+        type: 'donut'
+      },
+      labels: ['all','91'],
+      series: [100,42],
+      
+      colors: [
+        chartColors.donut.series1,
+        chartColors.donut.series4,
+        chartColors.donut.series3,
+        chartColors.donut.series2
+      ],
+      stroke: {
+        show: false,
+        curve: 'straight'
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function (val, opt) {
+          return parseInt(val, 10) + '%';
+        }
+      },colors: ["#c4c2c2", "#28c76f"],
+      legend: {
+        show: true,
+        position: 'bottom',
+        markers: { offsetX: -3 },
+        itemMargin: {
+          vertical: 3,
+          horizontal: 10
+        },
+        labels: {
+          colors: legendColor,
+          useSeriesColors: false
+        }
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+              name: {
+                fontSize: '2rem',
+                fontFamily: 'Public Sans'
+              },
+              value: {
+                fontSize: '1.2rem',
+                color: legendColor,
+                fontFamily: 'Public Sans',
+                formatter: function (val) {
+                  return parseInt(val, 10) + '%';
+                }
+              },
+              total: {
+                show: true,
+                fontSize: '1.5rem',
+                color: headingColor,
+                label: '91',
+                formatter: function (w) {
+                  return '42%';
+                }
+              }
+            }
+          }
+        }
+      },
+      responsive: [
+        {
+          breakpoint: 992,
+          options: {
+            chart: {
+              height: 380
+            },
+            legend: {
+              position: 'bottom',
+              labels: {
+                colors: legendColor,
+                useSeriesColors: false
+              }
+            }
+          }
+        },
+        {
+          breakpoint: 576,
+          options: {
+            chart: {
+              height: 320
+            },
+            plotOptions: {
+              pie: {
+                donut: {
+                  labels: {
+                    show: true,
+                    name: {
+                      fontSize: '1.5rem'
+                    },
+                    value: {
+                      fontSize: '1rem'
+                    },
+                    total: {
+                      fontSize: '1.5rem'
+                    }
+                  }
+                }
+              }
+            },
+            legend: {
+              position: 'bottom',
+              labels: {
+                colors: legendColor,
+                useSeriesColors: false
+              }
+            }
+          }
+        },
+        {
+          breakpoint: 420,
+          options: {
+            chart: {
+              height: 280
+            },
+            legend: {
+              show: false
+            }
+          }
+        },
+        {
+          breakpoint: 360,
+          options: {
+            chart: {
+              height: 250
+            },
+            legend: {
+              show: false
+            }
+          }
+        }
+      ]
+    };
+  if (typeof donutChartEle !== undefined && donutChartEle !== null) {
+    const donutCharte = new ApexCharts(donutChartEle, donutChartConfige);
+    donutCharte.render();
+  }
 })();
